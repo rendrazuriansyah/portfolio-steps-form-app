d59a15e4c36937af2c532dd6bb0cff35a5a289e3
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-07 18:53:19 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="steps">
+			<div className="numbers">
+				<div className="active">1</div>
+				<div className="">2</div>
+				<div className="">3</div>
+			</div>
+			<p className="message">Step: Dream</p>
+			<div className="buttons">
+				<button style={{ backgroundColor: "#526d82", color: "#fff" }}>
+					Prev
+				</button>
+				<button style={{ backgroundColor: "#526d82", color: "#fff" }}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
